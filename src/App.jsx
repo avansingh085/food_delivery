d50314f3979ce02aed90122920b22812115a9953
Avan Singh <avansingh085@gmail.com>
2025-01-16 17:17:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-slate-800'>
+        i am a box
+      </div>
+    </>
+  )
+}
+
+export default App
